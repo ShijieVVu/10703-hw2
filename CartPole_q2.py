2da482438924b6f342f1dd86565d118c817ff499
Shijie <wushijiedu@gmail.com>
2018-03-10 01:48:56 -0500

    Code and plot working

--- CartPole_q2.py
+++ CartPole_q2.py
@@ -5,6 +5,10 @@ from glob import glob
 identifier = "CartPole_q2"
 
 # start training
-dqn.main(env_name="CartPole-v0", identifier=identifier, max_iteration=1000000, epsilon=1.0, epsilon_decay=9.5e-6,
-         epsilon_min=0.05, interval_iteration=10000, gamma=0.99, test_size=20, learning_rate=0.0001,
-         use_replay_memory=True, memory_size=100000, burn_in=20000)
+# dqn.main(env_name="CartPole-v0", identifier=identifier, max_iteration=1000000, epsilon=1.0, epsilon_decay=9.5e-6,
+#          epsilon_min=0.05, interval_iteration=10000, gamma=0.99, test_size=20, learning_rate=0.0001,
+#          use_replay_memory=True, memory_size=100000, burn_in=20000)
+
+dqn.main(env_name="CartPole-v0", identifier=identifier, max_iteration=1000000, epsilon=0.5, epsilon_decay=4.5e-6,
+         epsilon_min=0.05, interval_iteration=10000, gamma=0.99, test_size=20, learning_rate=0.002,
+         use_replay_memory=False, memory_size=50000, burn_in=10000)
--- plot_performance.py
+++ plot_performance.py
@@ -1,8 +1,8 @@
 from matplotlib import pylab as plt
 from pickle import load
 
-name = 'CartPole_q2'
-# name = 'MountainCar_q2'
+# name = 'CartPole_q4'
+name = 'MountainCar_q4'
 
 data = load(open('./model/{}.p'.format(name), 'rb'))
 
@@ -12,4 +12,8 @@ plt.title(name)
 plt.xlabel('iteration')
 plt.ylabel('average reward')
 plt.plot(iteration, reward)
+if "CartPole" in name:
+    plt.plot(iteration, [195] * len(iteration))
+if "MountainCar" in name:
+    plt.plot(iteration, [-110] * len(iteration))
 plt.show()
